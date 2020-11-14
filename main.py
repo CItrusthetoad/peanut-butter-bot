2c07ccfe642e65f672f11224081da3bcc81043a2
CitrusRAWR <itsmegaplex@gmail.com>
2020-11-13 22:36:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+import discord
+import random
+import time
+import asyncio
+
+TOKEN = "NzY5MjkxNzc2MjI4ODUxNzIz.X5M4zA.9q64_Ctc9-Pw5R29_9bYybeqb8k"
+
+client = discord.Client()
+images = ["https://imgur.com/a/nVxLm9w", "https://imgur.com/a/LjmiVDJ", "https://imgur.com/a/D7f3ufu"]
+pb = ["https://imgur.com/a/nVxLm9w", "https://imgur.com/a/D7f3ufu"]
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith(".test"):
+        await message.channel.send("Shut the fuck up")
+
+    if message.content.startswith(".say"):
+        mes = message.content.split()
+        output = ""
+        for word in mes[1:]:
+            output += word
+            output += " "
+        await message.channel.send(output)
+
+    if message.content.startswith(".help"):
+        embed = discord.Embed(title="__Commands__", color=0x00ff00)
+        embed.add_field(name=".help", value="Shows this message")
+        embed.add_field(name=".say", value="Replies with whatever you said")
+        await message.channel.send(content=None, embed=embed)
+
+    if message.content.startswith(".blue"):
+        await message.channel.send("https://imgur.com/a/nVxLm9w")
+
+    if message.content.startswith(".orange"):
+        await message.channel.send("https://imgur.com/a/LjmiVDJ")
+
+    if message.content.startswith(".random"):
+        await message.channel.send(random.choice(images))
+
+    if message.content.startswith(".pb"):
+        await message.channel.send(random.choice(pb))
+
+
+
+
+@client.event
+async def on_ready():
+    print("RUNNING")
+
+client.run(TOKEN)
